ff19dbc18b1611833748c869d45a56ddb8f5a31e
Manisha Rayudu <83665305+manisharayudu@users.noreply.github.com>
2022-08-11 10:44:39 +0200

    Added IaC files

--- terraform_files/securitygroup.tf
+++ terraform_files/securitygroup.tf
@@ -19,12 +19,37 @@ resource "aws_security_group" "allow-alter-ssh" {
 
   ingress {
     description      = "Allow from Personal CIDR block"
-    from_port        = 80
-    to_port          = 80
+    from_port        = 8000
+    to_port          = 8000
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   }
   tags = {
     Name = "allow-alter-ssh"
+  }
+}
+
+#Security group for AWS ELB
+resource "aws_security_group" "alter-elb-securitygroup" {
+  vpc_id      = aws_vpc.Python_Task.id
+  name        = "alter-elb-securitygroup"
+  description = "security group for Elastic Load Balancer"
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alter-elb-securitygroup"
   }
 }
